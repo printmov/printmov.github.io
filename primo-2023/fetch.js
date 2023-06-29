28462c0f635baa00dc5893f290d3082fbdd447bc
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2023-06-29 17:07:51 +0700

    Update fetching

--- primo-2023/fetch.js
+++ primo-2023/fetch.js
@@ -4,7 +4,8 @@ $(window).on('load', function() {
 
 });
 
-var characters;
+var charactersJson;
+var dateJson;
 
 function fetchCharacters() {
   var d = new Date();
@@ -34,6 +35,25 @@ function fetchDate() {
     month = '0' + month;
   }
   $.getJSON("message-database.json", function(json) {
+    dateJson = json;
     console.log(json[day + '-' + month]);
+    render();
   });
+}
+
+function render() {
+  var character = charactersJson[dateJson.character];
+  var filename = character.path + character.filename + "-" + ((dateJson.count % character.variant) + 1 ) + ".png";
+  $(".character-image").attr("src", filename);
+  var bgFilename = character.path + "bg-" + (Math.floor(Math.random() * character.bg) + 1) + ".png";
+  $(".background").attr("src", bgFilename);
+  if(character["overlay"] > 0) {
+    var overlayFilename = character.path + "overlay-" + (Math.floor(Math.random() * character.overlay) + 1) + ".png";
+    $(".overlay").attr("src", overlayFilename);
+  }
+  else {
+    $(".overlay").hide();
+  }
+  $(".message").text(dateJson.message);
+  $(".black-hide").fadeOut(500);
 }
