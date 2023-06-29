0280624407421bc530575d227368fd88fb39b85d
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2023-06-29 16:27:32 +0700

    Update fetch.js

--- primo-2023/fetch.js
+++ primo-2023/fetch.js
@@ -1,10 +1,26 @@
 $(window).on('load', function() {
 
+  fetchCharacters();
   fetch();
 
 });
 
-function fetch() {
+function fetchCharacters() {
+  var d = new Date();
+  var month = d.getMonth() + 1;
+  var day = d.getDate();
+  if (day < 10) {
+    day = '0' + day;
+  }
+  if (month < 10) {
+    month = '0' + month;
+  }
+  $.getJSON("characters.json", function(json) {
+    console.log(json);
+  });
+}
+
+function fetchDate() {
   var d = new Date();
   var month = d.getMonth() + 1;
   var day = d.getDate();
